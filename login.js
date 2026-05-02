d1dd944513dddac00f1eac91650269a51bf75997
Bhumit3695 <bhumitreddybasireddy@gmail.com>
2026-05-02 14:58:40 -0500

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-  const firebaseConfig = {
-    apiKey: "AIzaSyBDe8lWKkhszxdX2u6-F9lwRtn_W5oGt8Y",
-    authDomain: "password-manager-b0.firebaseapp.com",
-    projectId: "password-manager-b0",
-    storageBucket: "password-manager-b0.firebasestorage.app",
-    messagingSenderId: "578861223039",
-    appId: "1:578861223039:web:3951ed8781b96d8fc1434a"
-  };
-
